52b54eeef0a91884ef3408230aa7ea5da3902167
tomatem <tem7720@gmail.com>
2023-04-24 00:26:12 +0300

    done

--- carrier-rocket.js
+++ carrier-rocket.js
@@ -1,9 +1,10 @@
 class CarrierRocket {
-  constructor(stages) {
-    
-  }
-
-
+    constructor(stages) {
+        this.stages = [...stages];
+    }
+    detachStage() {
+        this.stages = this.stages.slice(1);
+    }
 }
 
 module.exports = CarrierRocket;
--- rocket-engine.js
+++ rocket-engine.js
@@ -1,7 +1,8 @@
 class RocketEngine {
-  constructor(specificImpulse, rocketPropellant) {
-    
-  }
+    constructor(specificImpulse, rocketPropellant) {
+        this.specificImpulse = specificImpulse;
+        this.rocketPropellant = rocketPropellant;
+    }
 }
 
 module.exports = RocketEngine;
